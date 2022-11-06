fb64293c8ce1ba384cdbee6694a673a986df8156
timmy-oss <timileyinpelumi.dev@gmail.com>
2022-11-06 15:27:17 +0100

    IPFS JSON upload

--- models/settings.py
+++ models/settings.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     env: str = "development"
     db_url: str = 'mongodb://localhost:27017'
     token_expiration: int = 30
+    ca : str = "0x9251af0b56663032bee1715bbe8c0ed65fda2fc1"
     secret_key: str = "c3126bf2dc199fe90b3f50abc06e3a1afb899af286b073e966bc7927f79641f781482d52dcee6fe34c25b201faead36a5a617813f8ded6c7b699de2c30b3914b8e632fa4db3d1db1e6292e1b4b1ab217b9ff377982d23ea57ed1c2359ab0a33d5893f72173f0f270861b7681e8785997982bf4813a747bbf46ceffbef3d4eb07"
     hash_algorithm: str = 'HS256'
     client_url: str = 'https://www.fabaaw.com'
