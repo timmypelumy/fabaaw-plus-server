a9444b7fc09e9c860029d1bee2363fe6f61d54c2
Timileyin Pelumi <timileyinpelumi.dev@gmail.com>
2023-11-04 21:34:30 +0100

    Update settings.py

--- models/settings.py
+++ models/settings.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     ca : str = "0x9251af0b56663032bee1715bbe8c0ed65fda2fc1"
     secret_key: str = "c3126bf2dc199fe90b3f50abc06e3a1afb899af286b073e966bc7927f79641f781482d52dcee6fe34c25b201faead36a5a617813f8ded6c7b699de2c30b3914b8e632fa4db3d1db1e6292e1b4b1ab217b9ff377982d23ea57ed1c2359ab0a33d5893f72173f0f270861b7681e8785997982bf4813a747bbf46ceffbef3d4eb07"
     hash_algorithm: str = 'HS256'
-    client_url: str = 'https://www.fabaaw.com'
+    client_url: str = 'https://www.fabaawplus.com'
     ipfs_node_url: str = 'https://ipfs.infura.io:5001'
     infura_project_id: str = '2CASClsLixgaD7e6qlO5LfIYA4b'
     infura_project_secret: str = '3a6dfcb5e77b97ba69b90f55e1f7b326'
